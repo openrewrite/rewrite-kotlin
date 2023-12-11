6f50d6e81eee995f43f00cd415b9a2eb2c3eecbb
Kun Li <kun@moderne.io>
2023-12-11 13:22:31 -0800

    Address Jonathan's comment

--- src/main/java/org/openrewrite/kotlin/internal/KotlinTreeParserVisitor.java
+++ src/main/java/org/openrewrite/kotlin/internal/KotlinTreeParserVisitor.java
@@ -1148,7 +1148,7 @@ public class KotlinTreeParserVisitor extends KtVisitor<J, ExecutionContext> {
                 type
         ));
 
-        return delegationCall != null ? new K.Constructor(randomId(), Markers.EMPTY, methodDeclaration, null, null, padLeft(prefix(constructor.getColon()), delegationCall)) :
+        return delegationCall != null ? new K.Constructor(randomId(), Markers.EMPTY, methodDeclaration, padLeft(prefix(constructor.getColon()), delegationCall)) :
                 methodDeclaration;
     }
 
