d485b1dfcf9f5dd9746e84717c3c2b7584cc51a4
Tim te Beek <tim@moderne.io>
2024-02-11 14:41:34 +0100

    Replace print with assertion

--- src/test/java/org/openrewrite/kotlin/tree/ImportTest.java
+++ src/test/java/org/openrewrite/kotlin/tree/ImportTest.java
@@ -98,6 +98,7 @@ class ImportTest implements RewriteTest {
           kotlin(
             "import   createInstance /*C1*/",
             spec -> spec.afterRecipe(cu -> {
+                assertThat(cu.getImports().get(0).getPackageName()).isEmpty();
             })
           )
         );
