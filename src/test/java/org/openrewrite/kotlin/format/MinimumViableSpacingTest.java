f54ac1e0f52d69dda98e0b2e08aefc83e0b67afe
Tim te Beek <timtebeek@gmail.com>
2024-03-12 11:31:12 +0000

    refactor: Common static analysis issues
    Use this link to re-run the recipe: https://app.moderne.io/recipes/builder/ggZMncs1O?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/kotlin/format/MinimumViableSpacingTest.java
+++ src/test/java/org/openrewrite/kotlin/format/MinimumViableSpacingTest.java
@@ -211,7 +211,7 @@ class MinimumViableSpacingTest implements RewriteTest {
     void variableDeclarationsInClass2() {
         rewriteRun(
           spec -> spec.recipes(
-            toRecipe(() -> new MinimumViableSpacingVisitor<>())
+            toRecipe(MinimumViableSpacingVisitor::new)
           ),
           kotlin(
             """
@@ -312,7 +312,7 @@ class MinimumViableSpacingTest implements RewriteTest {
         rewriteRun(
           spec -> spec.parser(KotlinParser.builder().classpath("junit-jupiter-api"))
             .recipes(
-              toRecipe(() -> new MinimumViableSpacingVisitor<>())
+              toRecipe(MinimumViableSpacingVisitor::new)
             ),
           kotlin(
             """
@@ -332,7 +332,7 @@ class MinimumViableSpacingTest implements RewriteTest {
     void classConstructor() {
         rewriteRun(
           spec -> spec.recipes(
-            toRecipe(() -> new MinimumViableSpacingVisitor<>())
+            toRecipe(MinimumViableSpacingVisitor::new)
           ),
           kotlin(
             """
@@ -351,7 +351,7 @@ class MinimumViableSpacingTest implements RewriteTest {
     void spaceAfterPublic() {
         rewriteRun(
           spec -> spec.recipes(
-            toRecipe(() -> new MinimumViableSpacingVisitor<>())
+            toRecipe(MinimumViableSpacingVisitor::new)
           ),
           kotlin(
             """
@@ -369,7 +369,7 @@ class MinimumViableSpacingTest implements RewriteTest {
     void statementWithCommentInPrefix() {
         rewriteRun(
           spec -> spec.recipes(
-            toRecipe(() -> new MinimumViableSpacingVisitor<>())
+            toRecipe(MinimumViableSpacingVisitor::new)
           ),
           kotlin(
             """
@@ -387,7 +387,7 @@ class MinimumViableSpacingTest implements RewriteTest {
     void compilationUnitDeclarations() {
         rewriteRun(
           spec -> spec.recipes(
-            toRecipe(() -> new MinimumViableSpacingVisitor<>())
+            toRecipe(MinimumViableSpacingVisitor::new)
           ),
           kotlin(
             """
@@ -404,7 +404,7 @@ class MinimumViableSpacingTest implements RewriteTest {
     void blockWithImplicitReturn() {
         rewriteRun(
           spec -> spec.recipes(
-            toRecipe(() -> new MinimumViableSpacingVisitor<>())
+            toRecipe(MinimumViableSpacingVisitor::new)
           ),
           kotlin(
             """
--- src/test/java/org/openrewrite/kotlin/format/RemoveTrailingWhitespaceTest.java
+++ src/test/java/org/openrewrite/kotlin/format/RemoveTrailingWhitespaceTest.java
@@ -29,7 +29,7 @@ class RemoveTrailingWhitespaceTest implements RewriteTest {
 
     @Override
     public void defaults(RecipeSpec spec) {
-        spec.recipe(toRecipe(() -> new RemoveTrailingWhitespaceVisitor<>()));
+        spec.recipe(toRecipe(RemoveTrailingWhitespaceVisitor::new));
     }
 
     @DocumentExample
