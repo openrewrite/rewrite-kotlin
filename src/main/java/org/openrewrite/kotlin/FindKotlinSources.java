2a656628da8abb0441b8202940b6a834ecd5732f
Jonathan Schnéider <jkschneider@gmail.com>
2024-08-15 02:50:35 +0000

    Migrate to JSpecify from OpenRewrite JSR-305 meta-annotations
    Use this link to re-run the recipe: https://app.moderne.io/builder/lFW7wUiCk?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/openrewrite/kotlin/FindKotlinSources.java
+++ src/main/java/org/openrewrite/kotlin/FindKotlinSources.java
@@ -17,8 +17,8 @@ package org.openrewrite.kotlin;
 
 import lombok.EqualsAndHashCode;
 import lombok.Value;
+import org.jspecify.annotations.Nullable;
 import org.openrewrite.*;
-import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.kotlin.table.KotlinSourceFile;
 import org.openrewrite.kotlin.tree.K;
 import org.openrewrite.marker.SearchResult;
